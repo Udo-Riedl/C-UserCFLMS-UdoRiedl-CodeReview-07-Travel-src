34cc74da3eaf435ba291b7e1ee80a4a7f5496f09
Udo Riedl <uriedl@gmx.at>
2020-06-27 16:31:30 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { BlogPageComponent } from './blog-page/blog-page.component';
+import { TravelsPageComponent } from './travels-page/travels-page.component';
+import { CartComponent } from './cart/cart.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",component: HomePageComponent
+},
+{
+    path: "blog",component: BlogPageComponent
+},
+{
+  path: "travels",component: TravelsPageComponent
+},
+{
+  path: 'cart', component: CartComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { BlogPageComponent } from './blog-page/blog-page.component';
+import { TravelsPageComponent } from './travels-page/travels-page.component';
+import { HeroComponent } from './hero/hero.component';
+import { TravelsListComponent } from './travels-list/travels-list.component';
+import { TravelsDetailsComponent } from './travels-details/travels-details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomePageComponent,
+    BlogPageComponent,
+    TravelsPageComponent,
+    HeroComponent,
+    TravelsListComponent,
+    TravelsDetailsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
